15b07aa05c14cd6211a192a6e6da43ed7427334f
Anis <me.anis.dev@gmail.com>
2024-10-28 20:30:46 +0000

    🗃 DB connection

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,29 @@
-from flask import Flask
+import mysql.connector
+from flask import Flask , g
 from .config import Config
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+
+    def connect_db():
+        return mysql.connector.connect(
+            host=app.config['DB_HOST'],
+            user=app.config['DB_USER'],
+            password=app.config['DB_PASSWORD'],
+            database=app.config['DB_NAME'],
+            port=app.config['DB_PORT']
+        )
+    
+    @app.before_request
+    def before_request():
+        g.db = connect_db()
+
+    @app.teardown_request
+    def teardown_request(exception):
+        db = getattr(g, 'db', None)
+        if db is not None:
+            db.close()
 
     @app.route('/')
     def index():
